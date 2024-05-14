4e4050200f66bbd4f554a766432ea1d836f24165
rxfe-a <28RafaelA539@gmail.com>
2024-05-13 23:52:42 -0700

    start

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({
+    storage: storage
+}).single('file');
+app.use(express.static(path.join(__dirname, 'public')));
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
+            res.status(400).send('Error uploading file.');
+        } else {
+            res.send('File uploaded!');
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
